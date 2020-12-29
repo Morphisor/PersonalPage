9730edc10596c01917e32d31ba302f94783341a6
Morphisor <a.gallitri@gmail.com>
2020-12-29 11:16:29 +0000

    using cdn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const basePath = process.env.NODE_ENV === 'production' ? '/PersonalPage' : '';
+const assetPrefix = process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/Morphisor/PersonalPage/gh-pages/' : ''
 //const basePath = '/PersonalPage';
 
 module.exports = {
   basePath,
-  assetPrefix: `${basePath}/`
+  assetPrefix
 };
